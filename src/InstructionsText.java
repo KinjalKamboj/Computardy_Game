5cd362ddaaf3bdfd0370a5b3068c0b0c998a1176
Kinjal Kamboj <85907315+KinjalKamboj@users.noreply.github.com>
2022-05-26 07:17:15 -0400

    Updated Instructions.java

--- src/InstructionsText.java
+++ src/InstructionsText.java
@@ -24,7 +24,7 @@ public class InstructionsText extends Instructions {
    
 	public String getInstructionsText() {
 		text = new JPanel();
-		/*textFile = new File("Instructions.txt");
+		textFile = new File("Instructions.txt");
 		try {           
 			intake = new FileReader (textFile);
 			readFile = new BufferedReader (intake);
@@ -43,19 +43,4 @@ public class InstructionsText extends Instructions {
 }
 		return textRead;
 	}
-	}*/
-		textRead += "Welcome to Computardy! This game is a single-player review game \r\n"
-				+ "designed to allow you to review important concepts in Grade 12 \r\n"
-				+ "Computer Science. To get started, please go back and click on the \r\n"
-				+ "start buzzer button. You will be directed to the main game where \r\n"
-				+ "there are 12 questions, three in each category, ranging from \r\n"
-				+ "easy (150), medium (250) and hard (350). Select a question \r\n"
-				+ "and choose the best answer from each multiple choice. You have \r\n"
-				+ "45 seconds to answer each question. It would be ideal to achieve\r\n"
-				+ "a minimum score of 2050 points. You can choose to exit the \r\n"
-				+ "game anytime by clicking the exit button or closing the frame. \r\n"
-				+ "		   Good luck!";
-		return textRead;
-	}
 }
-
